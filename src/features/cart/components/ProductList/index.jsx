28d1ad1323fdaf2e1869ec42965f9a442cfa36ba
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-01 00:05:03 +0700

    handle AddToCart function

--- src/features/cart/components/ProductList/index.jsx
+++ src/features/cart/components/ProductList/index.jsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useRecoilState } from "recoil";
 import { productListState } from "../../productState";
+import { cartState, addToCart } from "../../cartState";
 
 ProductList.propTypes = {};
 
 function ProductList() {
 	const productList = useRecoilValue(productListState);
+	const [cart, setCart] = useRecoilState(cartState);
+
+	const handleAddToCart = (product) => {
+		const newCart = addToCart(cart, product);
+		setCart(newCart);
+	};
+
 	return (
 		<div>
 			<h2>Product List</h2>
 
 			<ul className="product-list">
 				{productList.map((product) => {
-					return <li key={product.id}>{product.title}</li>;
+					return (
+						<li key={product.id}>
+							{product.title} - {product.price}
+							<button
+								style={{ marginLeft: "10px" }}
+								onClick={() => handleAddToCart(product)}
+							>
+								Add to Cart
+							</button>
+						</li>
+					);
 				})}
 			</ul>
 		</div>
