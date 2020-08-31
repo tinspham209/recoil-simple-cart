888d0464c195a886609dcf91f05e15ab46d60669
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-31 23:52:57 +0700

    setup RecoilRoot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
 	<React.Fragment>
-		<App />
+		<RecoilRoot>
+			<App />
+		</RecoilRoot>
 	</React.Fragment>,
 	document.getElementById("root")
 );
