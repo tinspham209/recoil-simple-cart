aa7b75cb2b81dfd3d6d95af1bf4955aac77041d5
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-31 23:56:07 +0700

    render productList from productListState

--- src/features/cart/components/ProductList/index.jsx
+++ src/features/cart/components/ProductList/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { productListState } from "../../productState";
 
 ProductList.propTypes = {};
 
 function ProductList() {
+	const productList = useRecoilValue(productListState);
 	return (
 		<div>
 			<h2>Product List</h2>
+
+			<ul className="product-list">
+				{productList.map((product) => {
+					return <li key={product.id}>{product.title}</li>;
+				})}
+			</ul>
 		</div>
 	);
 }
