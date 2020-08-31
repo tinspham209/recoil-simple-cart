ba9f8f538b45799ba8cdbc94a4d08601228acd03
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-01 00:09:38 +0700

    cartTotal function with selector

--- src/features/cart/cartState.js
+++ src/features/cart/cartState.js
@@ -1,8 +1,18 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const cartState = atom({
 	key: "cart",
 	default: [],
+});
+
+export const cartTotal = selector({
+	key: "cartTotal",
+	get: ({ get }) => {
+		const cart = get(cartState);
+		return cart.reduce((total, item) => {
+			return total + item.product.price * item.quantity;
+		}, 0);
+	},
 });
 
 export const addToCart = (cart, product) => {
